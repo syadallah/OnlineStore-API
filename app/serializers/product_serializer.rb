ec8ca685b1e45ed872b0f1255f8da87e230af3f5
syadallah <syadallah@gmail.com>
2020-03-11 10:15:54 -0400

    Add :user_id to the product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :editable
+  attributes :id, :name, :description, :price, :editable, :user_id
 
   def editable
     scope == object.user
